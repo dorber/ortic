3c17d989e044b653d0aa3a0b359825b60a1b506e
dorber <dorber@afkxxx.com>
2018-03-26 12:22:12 +0300

    Code updated

--- psr/basic/definitions/di.php
+++ psr/basic/definitions/di.php
@@ -4,7 +4,7 @@ use \Psr\Container\ContainerInterface;
 
 return [
     \FastRoute\Dispatcher::class => function (ContainerInterface $c) {
-        return FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+        return FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) use ($c) {
             require __DIR__ . '/routes.php';
         });
     },
--- psr/basic/definitions/routes.php
+++ psr/basic/definitions/routes.php
@@ -8,10 +8,11 @@ function response() { return new \Slim\Http\Response(); }
 
 /**
  * @var \FastRoute\RouteCollector $r
+ * @var \Psr\Container\ContainerInterface|\Anonymous\SimpleDi\FactoryInterface $c
  */
 
-$r->get('/', function () {
-    return response()->write('Hello, World!');
+$r->get('/', function () use ($c) {
+    return $c->instantiate(\Slim\Http\Response::class)->write('Hello, World!');
 });
 
 $r->get('/{name:[a-zA-Z0-9_-]+}', function (ServerRequestInterface $request) {
